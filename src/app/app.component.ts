3a4d89625ef5a4199bb03cfe18aa995b047b9fcf
Milosz Mertka <milosz.mertka@onet.pl>
2023-11-09 19:43:24 +0100

    Added versioning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pw-mwo-lab-integration';
+  version = packageJson.version;
 }
